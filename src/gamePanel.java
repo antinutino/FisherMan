08268b2706e0cefa005642cee051f850598c2315
antinutino <maharabopi73@gmail.com>
2024-07-05 22:21:24 +0600

    fisherman added

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -8,17 +8,37 @@ public class gamePanel extends JPanel implements ActionListener {
     final int panel_Height = 480;
     Image backgroundImage;
     Image fish1mirror;
+    Image fisherman1;
+    Image fisherman1tookpositon;
+    Image fisherman1catchedfish;
+    Image fisherman1missedfish;
+    Image fisherman1withoutbait;
+
     Image[] fishImages;
-    int[] fishX = {400, 250, 500, 280, 350, 560};
-    int[] fishY = {300, 300, 280, 350, 250, 260};
-    boolean[] movingLeft = {true, true, true, true, true, true};
+
+    int[] fishX = {400, 250, 500, 280, 340, 650,100};
+    int[] fishY = {300, 300, 280, 350, 250, 255,248};
+    boolean[] movingLeft = {true, true, true, true, true, true,true};
     Timer timer;
-    final int numFish = 6;
+    final int numFish = 7;
+    JButton button1,button2;
 
     gamePanel() {
         this.setPreferredSize(new Dimension(panel_Width, panel_Height));
+        this.setLayout(null);
+        // Use null layout to position components manually
+//      JPanel Panel2=new JPanel();
+//       Panel2.setLayout(null);
+//        Panel2.setBounds(225, 270, 5, 5); // Fixed position and size for nested panel
+//        Panel2.setBackground(Color.RED);
+//       this.add(Panel2);
         try {
             backgroundImage = new ImageIcon(getClass().getResource("/frame.png")).getImage();
+            fisherman1tookpositon = new ImageIcon(getClass().getResource("/fisherman1tookposition.png")).getImage();
+            fisherman1catchedfish = new ImageIcon(getClass().getResource("/fisherman1catchedfish.png")).getImage();
+            fisherman1missedfish = new ImageIcon(getClass().getResource("/fisherman1missedfish.png")).getImage();
+            fisherman1withoutbait = new ImageIcon(getClass().getResource("/fisherman1withoutbait.png")).getImage();
+            fisherman1=fisherman1tookpositon;
             fishImages = new Image[numFish];
             for (int i = 0; i < numFish; i++) {
                 fishImages[i] = new ImageIcon(getClass().getResource("/fish1.png")).getImage();
@@ -28,6 +48,16 @@ public class gamePanel extends JPanel implements ActionListener {
             System.out.println("Error loading image: " + e.getMessage());
         }
         this.setBackground(Color.black);
+
+        // Create and add button
+        button1 = new JButton("PULL");
+        button2=new JButton("USE Bait");
+        button1.setBounds(200, 440, 80, 30);
+        button2.setBounds(320,440,100,30);// Set button position and size
+        button1.addActionListener(new ButtonClickListener());
+        button2.addActionListener(new ButtonClickListener());
+        this.add(button2);
+        this.add(button1);
 
         timer = new Timer(100, this);
         timer.start();
@@ -39,6 +69,9 @@ public class gamePanel extends JPanel implements ActionListener {
         Graphics2D g2D = (Graphics2D) g;
         if (backgroundImage != null) {
             g2D.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
+        if (fisherman1 != null) {
+            g2D.drawImage(fisherman1, 40, 10, 200, 300, this);
         }
         for (int i = 0; i < numFish; i++) {
             if (fishImages[i] != null) {
@@ -82,4 +115,40 @@ public class gamePanel extends JPanel implements ActionListener {
     private int getRightBoundary(int y) {
         return panel_Width;
     }
+
+    private class ButtonClickListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Perform action on button click
+            if(e.getSource()==button1)
+            {  if(fisherman1==fisherman1tookpositon)
+                catchingfish();
+            }
+            else{
+                usingbait();
+            }
+        }
+    }
+
+    public void catchingfish() {
+        boolean checkfishcatching=true;
+        // Toggle fisherman image between fisherman1 and fisherman2
+        for(int i=4;i<7;i++)
+        {
+            if((fishX[i]>=220&&fishX[i]<=230)&&movingLeft[i]) {
+                    fisherman1 = fisherman1catchedfish;
+                    fishX[i] = 650;
+                    checkfishcatching=false;
+            }
+        }
+           if(checkfishcatching)
+               fisherman1=fisherman1missedfish;
+        repaint();
+    }
+    public void usingbait(){
+
+         fisherman1=fisherman1tookpositon;
+        repaint();
+    }
+
 }
